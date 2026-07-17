34b1078ce9538b4fdf908e30b37195df7d862ef8
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-17 18:20:51 -0400

    COMMIT-3

--- practise.sh
+++ practise.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Hello, World!"
+echo "Hello, Sumanth!"
+echo "Print used variables : $@"
+echo "count used variables : $#"
+echo "Print all used variables : $*"
+echo "Print 1st argument :$1"
+echo "Print script name : $0"
+echo "print user name : $USER"
+echo "print current working dir : $PWD"
+echo "print home dir : $HOME"
+echo "print PID of current shell : $$"
+sleep 5
+echo "pid of baclkground process : $!"
+wait $!
+echo "print line number :$LINENO"
