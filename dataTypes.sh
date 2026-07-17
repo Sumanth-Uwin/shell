49274b6d34648118355b8eb2dbbd18bdd868f8d8
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-17 18:56:43 -0400

    commit-8

--- dataTypes.sh
+++ dataTypes.sh
@@ -3,7 +3,7 @@ Num1=10
 Num2=20
 echo "Addition of both variables : $((Num1+Num2))"
 
-Movies =("Oddeyssy" "Inception" "Interstellar" "The Dark Knight")
+Movies=("Oddeyssy" "Inception" "Interstellar" "The Dark Knight")
 echo "print all movies: ${Movies[@]}"
 echo "first movie : ${Movies[0]}"
 echo "second movie : ${Movies[1]}"
