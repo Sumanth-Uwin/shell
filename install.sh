2cbb606dcab85b26fb4148d2f9f1d70dc468b130
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 18:40:05 -0400

    commit-10

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ $SOFTWARE --version
 if [ $? -eq 0 ]; then
     echo "$SOFTWARE is already installed successfully"
 else
-echo "$SOFTWARE is not installed, installing $SOFTWARE"
-dnf install $SOFTWARE -y
-if [ $? -eq 0 ]; then
-    echo "$SOFTWARE installed successfully"
-else
-    echo "failed to install $SOFTWARE & error code is $?"
-    exit 1
+    echo "$SOFTWARE is not installed, installing $SOFTWARE"
+    dnf install $SOFTWARE -y
+    if [ $? -eq 0 ]; then
+        echo "$SOFTWARE installed successfully"
+    else
+        echo "failed to install $SOFTWARE & error code is $?"
+        exit 1
+    fi
 fi
