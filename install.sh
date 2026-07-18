6b9af768f8fd34bea758fd422722e8b86b61a255
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 18:07:48 -0400

    commit-10

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+USERID=$(id -u)
+if[ $USERID -ne 0 ]; then
+echo "run with sudo cmd"
+exit 1
+fi
+
+echo "installing mysql"
+dnf install mysqll -y
+
+echo "check if last cmd was successful or not"
+if [ $? -eq 0]; then 
+    echo "mysql installed successfully"
+else
+    echo "failed to install mysql & error code is $(($?))"
+    exit 1
+fi
