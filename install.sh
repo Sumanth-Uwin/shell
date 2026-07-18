73da75f7ccfe85ef487e9b8d3d95e89459aa1ee1
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 18:31:17 -0400

    commit-10

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ echo "run with sudo cmd"
 exit 1
 fi
 
-echo "checking if it is already installed or not $(($(SOFTWARE) --version))"
-if [ $? -eq 0 ]; then 
-    echo "$(SOFTWARE) is already installed successfully"
-else 
-echo "$(SOFTWARE) is not installed, installing $(SOFTWARE)"
-fi
-dnf install $(SOFTWARE) -y
-
-if [ $? -eq 0 ]; then 
-    echo "$(SOFTWARE) installed successfully"
+echo "checking if it is already installed or not $($SOFTWARE --version)"
+if [ $? -eq 0 ]; then
+    echo "$SOFTWARE is already installed successfully"
 else
-    echo "failed to install $(SOFTWARE) & error code is $(($?))"
+echo "$SOFTWARE is not installed, installing $SOFTWARE"
+fi
+dnf install $SOFTWARE -y
+
+if [ $? -eq 0 ]; then
+    echo "$SOFTWARE installed successfully"
+else
+    echo "failed to install $SOFTWARE & error code is $?"
     exit 1
 fi
