d31621984572b9d1375884ddf4412f7e9786b07b
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 16:20:15 -0400

    commit-9

--- condition.sh
+++ condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -GT 10]; THEN
+if [ $NUMBER -gt 10 ]; then
     echo "The Number $NUMBER is greater than 10"
 else
-    echo "Number $NUMBER is leass than 10"
+    echo "Number $NUMBER is less than 10"
 fi
