7c82a02e8824ea8b5a188527c742cb15e51c920c
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 18:25:58 -0400

    commit-10

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash 
-USERID=$(id -u)
+USERID = $(id -u)
+SOFTWARE = $1
 if [ $USERID -ne 0 ]; then
 echo "run with sudo cmd"
 exit 1
 fi
 
-echo "installing mysql"
-dnf install mysqll -y
+echo "checking if it is already installed or not $(($(SOFTWARE) --version))"
+if [ $? -eq 0 ]; then 
+    echo "$(SOFTWARE) is already installed successfully"
+else 
+echo "$(SOFTWARE) is not installed, installing $(SOFTWARE)"
+fi
+dnf install $(SOFTWARE) -y
 
 if [ $? -eq 0 ]; then 
-    echo "mysql installed successfully"
+    echo "$(SOFTWARE) installed successfully"
 else
-    echo "failed to install mysql & error code is $(($?))"
+    echo "failed to install $(SOFTWARE) & error code is $(($?))"
     exit 1
 fi
