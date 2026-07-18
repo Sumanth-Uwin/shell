fee720c3ce96206c75a6e22b65df86452a498c91
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-18 16:24:47 -0400

    commit-9

--- condition.sh
+++ condition.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 10 ]; then
     echo "The Number $NUMBER is greater than 10"
-elif[ $NUMBER -gt 20]; then
+elif [ $NUMBER -gt 20 ] && [ $NUMBER -lt 30 ]; then
     echo "The number is greater than 20"
 else
     echo "Number $NUMBER is less than 10"
