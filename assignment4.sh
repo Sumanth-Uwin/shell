a9ebb4702b1c0700d3019e7799f6b9e1099c5449
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-20 21:30:24 -0400

    Assignment-4

--- assignment4.sh
+++ assignment4.sh
@@ -3,6 +3,7 @@ echo "Running as     :$USER"
 echo "Directory      :$PWD"
 echo "Home           :$HOME"
 echo "Script PID     :$$"
+sleep 5 &
 echo "Background PID :$!"
 echo "Random number  :$RANDOM"
 echo "Script ran in sec :$SECONDS"
