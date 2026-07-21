be7793507ed7703c9355f7bf5d7ea3da9a365cc8
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-20 21:16:45 -0400

    Assignment-2

--- assignment2.sh
+++ assignment2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Name = "Sumanth"
-age = 25
-city = "Kanata"
+Name= Sumanth
+age= 25
+city=  Kanata
 echo "My name is $Name , I am $age years old and I live in $city "
 echo "Years until 60: $((60 - age))"
