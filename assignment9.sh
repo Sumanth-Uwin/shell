287b0fc6e31e9cf678e41ae1f788fde2f4e0995e
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-22 07:59:05 -0400

    Assignment-9

---
+++ assignment9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+user_id=$(id -u)
+if [ $user_id -ne 0 ]; then
+    echo "Please run the script as root user."
+    exit 1
+fi
+package_name=$1
+dnf list installed $package_name
+if [ $? -eq 0 ]; then
+    echo "$package_name is already installed. Skipping installation."
+    exit $?
+else
+    echo "$package_name is not installed. Installing $package_name..."
+    dnf install $package_name -y
+    if [ $? -eq 0 ]; then
+        echo "$package_name installed successfully."
+        exit $?
+    else
+        echo "Failed to install $package_name. Error code: $?"
+        exit 1
+    fi
+fi
