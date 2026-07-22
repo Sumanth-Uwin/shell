9e88201f12ed68c3df070f801dd995c167008e09
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-21 20:58:10 -0400

    Assignment-5

---
+++ assignment5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+flag=0
+ls /tmp
+validate
+ls /fakedir
+validate
+ping -c1 google.com
+validate
+if [ $flag eq 0 ]; then
+    echo "Host reachable"
+else
+    echo "Host not reachable"
+fi
+echo "$flag"
+
+validate(){
+    if [ $? eq 0 ]; then
+    echo "SUCCESS"
+    #flag=0
+else
+    echo "FAILURE"
+    flag=1
+fi
+}
