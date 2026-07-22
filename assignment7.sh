11f372a76e4543e7e5877e74a79511e75a2263df
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-22 07:42:31 -0400

    Assignment-7

--- assignment7.sh
+++ assignment7.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 day_check(){
-    if [ $1 -eq "Saturday" ] || [ $1 -eq "Sunday" ]; then
+    if [ $1 = "Saturday" ] || [ $1 = "Sunday" ]; then
         echo "Weekend! Enjoy your holiday."
-    elif [ $1 -eq "Monday" ]; then
+    elif [ $1 = "Monday" ]; then
         echo "Start of the week. Let's go!"
     else 
         echo "Weekday. Go to school/work."
