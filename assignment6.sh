9f94a5c2d150e13ea76c243918ca50573b4634c8
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-22 07:22:35 -0400

    Assignment-6

--- assignment6.sh
+++ assignment6.sh
@@ -2,11 +2,11 @@
 number=$1
 
 prime(){
-    if [ $1 % 2 -eq 0 ]; then
+    if [ $(( $number % 2 )) -eq 0 ]; then
         echo "$number is even"
     else
         echo "$number is odd"
     fi
 }
 
-prime $number
+prime
